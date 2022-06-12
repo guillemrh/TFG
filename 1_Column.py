a6c32032684de9b951370ce1c39fb2073b548e03
guillemrh <guillemmm.98@gmail.com>
2022-06-12 12:01:29 +0200

    1 Column

--- 1_Column.py
+++ 1_Column.py
@@ -1,12 +1,11 @@
 #%%
 """
-First of all several functions are defined to create the samples (Latin Hypercube Sampling) and the connection HYSYS-python (UnisimConnection(object)).
+First of all several functions are defined to create the samples (Latin Hypercube Sampling) and the connection HYSYS-python (HysysConnection(object)).
 Then, samples are randomly generated and ordered, so HYSYS feasibility is improved.
 Finally, the connection is created and samples are simulated. The results are eventually exported to an Excel file through a pandas DataFrame.
 """
-### Functions to create the Sampling ###
+### Functions to create the Sampling data ###
 
-#%% Define the Latin Hypercube Sampling functions
 import numpy as np
 import matplotlib.pyplot as plt
 import array
@@ -31,12 +30,8 @@ def latin_hypercube_sampling(LOW,UP,p,d):
   return p
 
 
-
-### Functions to create the interface ###
-
-
 #pip install cognite-sdk
-#%% Imports
+
 import numpy as np
 import pandas as pd
 import win32com.client
@@ -50,10 +45,11 @@ from pathlib import Path
 from win32com.client import makepy 
 import matplotlib.pyplot as plt
 import sys
-class UnisimConnection(object):
-    ''' All the functions related to Unisim communication '''
-    #Open Unisim Case
-    def __init__(self,filepath,unisimpath):
+
+class HysysConnection(object):
+    ''' All the functions related to Hysys communication '''
+    #Open Hysys Case
+    def __init__(self,filepath,Hysyspath):
         '''
         Set all the global variables and do the convertion for C classes to Python language.
         The CLSID may be wrong because this script was written for Unisim in case is wrong we can look for the good ones
@@ -62,18 +58,18 @@ class UnisimConnection(object):
             self.casepath = filepath
             self.uniapp = win32com.client.Dispatch("HYSYS.Application.NewInstance")
             self.simcase = self.uniapp.SimulationCases.Open(filepath)
-            ## Call to Unisim backdor class on the register
+            ## Call to Hysys backdor class on the register
             self.BackDoor=makepy.gencache.GetClassForCLSID("{11FE7386-A1D1-4E47-B46A-8A09A7CE5EB2}")
             # Define class for FixedDataTables, Datatalbe and VarDefinition lybraries
             self.FixedDataTables = makepy.gencache.GetClassForCLSID("{4DEF63F0-103B-11D1-A455-00A0C923285C}")
             self.DataTable = makepy.gencache.GetClassForCLSID("{FE57ADBD-5DFA-46DA-B205-423FBF3799AA}")
             self.VarDefinition = makepy.gencache.GetClassForCLSID("{B37CCBF0-E29E-11D0-A418-00A0C923285C}")
-            sys.argv = ["makepy", unisimpath]
+            sys.argv = ["makepy", Hysyspath]
             makepy.main ()
             return
         except:
             assert self.simcase is not None, "Hysys Case is null"
-            UnisimConnection.KillUnisim(self.simcase)
+            HysysConnection.KillHysys(self.simcase)
 
     def OpenCase(self):
         ''' Open Hysys case & Activate the select case '''
@@ -81,26 +77,26 @@ class UnisimConnection(object):
         self.simcase.Activate()
         return [self.simcase,self.uniapp]
 
-    def KillUnisim(self,simCase = ''):
-        ''' Kill Hysys at the end of the run or when somethin goes wrong '''
+    def KillHysys(self,simCase = ''):
+        ''' Kill Hysys at the end of the run or when something goes wrong '''
         if self.simcase != '': 
             self.simcase.Close(False)
         return self.uniapp.Quit()   
                     
     def ReadDataTable(self,TableName):
         ''' Read Information from Process data table and convert it to a python dictionary'''
-        # Look for the Process Data Table of the Unisim case
+        # Look for the Process Data Table of the Hysys case
         DataTables = self.FixedDataTables(self.simcase.DataTables)
         ProcessDataTable = self.DataTable(DataTables.Item(TableName))
         ProcessDataTable.StartTransfer()
-        vd = ProcessDataTable.GetAllVarDefinitions()    #Això hauria de ser un diccionari, no?        
+        vd = ProcessDataTable.GetAllVarDefinitions()  
         # Extract all Tags info and save it into a dict
         DataTable = {}
         idict = {}
         Values = []
         for x,value in enumerate(vd):
-            vd = self.VarDefinition(value)  #Aquests son els values del diccionari final
-            idict[vd.Tag] = vd              #Dins del GetValue es podrien especificar les unitats en que volem que ho llegeixi.
+            vd = self.VarDefinition(value)  
+            idict[vd.Tag] = vd              
             Values.append(vd.Variable.GetValue())
         ProcessDataTable.EndTransfer()  
         DataTable[TableName] = idict
@@ -108,15 +104,12 @@ class UnisimConnection(object):
 
     def WriteTagsDataTable(self, TableDict,Values,dataTableName):
         '''Write all tags to selected process data table'''
-        #try:
-            #ProcessDataTable.StartTransfer()
-            # Write all the values back to the ProcessDataTable 
+    
+        # Write all the values back to the ProcessDataTable 
         DataTables = self.FixedDataTables(self.simcase.DataTables)
         ProcessDataTable = self.DataTable(DataTables.Item(dataTableName))
         ProcessDataTable.StartTransfer()
         getvalues = ProcessDataTable.GetAllValues()
-        #print('Inputs: ',type(getvalues))
-        #print(type(tuple(Values)))
         for iDataTable in TableDict:
             tempDict = TableDict[iDataTable]
             dictindex = 0
@@ -127,25 +120,21 @@ class UnisimConnection(object):
                         vd.Variable.SetValue(Values[dictindex], vd.Units) 
                 dictindex +=1
         ProcessDataTable.EndTransfer()
-        #except:
-            #print("Error writing tags")
+
     def Reset(self): 
-        ''' Runs the Unisim simulation from a script previously defined'''
+        ''' Resets the Hysys simulation from a script previously defined'''
         self.uniapp.PlayScript(r'C:\Users\vdi.eebe\Desktop\TFG-main\ResetColumnPython.scp')
         return True
 
     def Run(self): 
-        ''' Runs the Unisim simulation from a script previously defined'''
-        # References to BackDoor Variables
-        #PropPkgBd = self.BackDoor(self.simcase)
-        #PropPkgBd.SendBackDoorMessage('"FlowSht.1/UnitOpObject.400(A-BCD)/FlowSht.600" "Run"')
+        ''' Runs the Hysys simulation from a script previously defined'''
         self.uniapp.PlayScript(r'C:\Users\vdi.eebe\Desktop\TFG-main\RunColumnPython.scp')
-        'This sleep is jut to let the optimizer calculate, it can be checked also with a While loop'
+        'This sleep is just to let the optimizer calculate, it can be checked also with a While loop'
         #time.sleep(5) 
         return True
 
     def Calculate(self): 
-        ''' Call to Unisim optimizer '''
+        ''' Call to Hysys optimizer '''
         # References to BackDoor Variables
         PropPkgBd = self.BackDoor(self.simcase)
         PropPkgBd.SendBackDoorMessage('Optimizer.300:Execute')
@@ -155,12 +144,11 @@ class UnisimConnection(object):
 
 #%% Create the sampling points.
 #Define the upper and lower intervals for each variable. Remember that each position "i" in vectors LOW and UP matches each variable "i" in array p.
-#Model 1
-Inputs = ['NT_T1', 'D_T1', 'RR_T1']
+Inputs = ['NT_T5', 'D_T5', 'RR_T5']
 UP  = [18, 130 , 1.5]
 LOW = [10, 70, 1]
 
-n = 1000     #Number of samples that are required
+n = 10000     #Number of samples that are required
 d = len(UP)   #Number of inputs that are required
 
 #The array "p" is normalized between 0-1.
@@ -168,9 +156,7 @@ p = latin_hypercube_normalized(d,n)
 
 #The array "q" is not normalized. These are the sampling points. 
 q = latin_hypercube_sampling(LOW,UP,p,d)
-q[:,0]=q[:,0].astype(int)                           #Always keep the first input as NT (number of trays), as it must be a natural number (int).
-#q[:,1:]=np.around(q[:,1:],4)                         # We want the other variables to have 4 decimals, as this is the length in HYSYS variables.
-#q=np.around(q,4)                                     # We want all the other variables to have 4 decimals, as this is the length in HYSYS variables.
+q[:,0]=q[:,0].astype(int)                           #Always keep the first input  NT (number of trays) as a natural number (int).
 #%% Plot the sampling points to check everything is random
 x=2
 y=1
@@ -192,44 +178,36 @@ for i in np.arange(LOW[y], UP[y], 1/n*(UP[y]-LOW[y])):
 plt.show()
 
 #%%
-q= sorted(sorted(sorted(q,key=lambda x: x[1]),key=lambda x: -x[2]),key=lambda x: -x[0])   #Though the sample is random, the points are ordenated to reduce HYSYS hysteresis
-#sorted(q, key=lambda x: -x[0])
-#q
+q= sorted(sorted(sorted(q,key=lambda x: x[1]),key=lambda x: -x[2]),key=lambda x: -x[0])   #Though the sample is random, the points are sorted to reduce HYSYS hysteresis
 #%% Sample the data points
 
-filepath   =r'C:\Users\vdi.eebe\Desktop\TFG-main\PE.hsc'  # Ubicació de la simulació a mapejar
-unisimpath =r'C:\Program Files\AspenTech\Aspen HYSYS V12.0\hysys.tlb'  # Ubicació de la instal·lació de HYSYS 
+filepath   =r'C:\Users\vdi.eebe\Desktop\TFG-main\PE.hsc'  # HYSYS simulation
+Hysyspath =r'C:\Program Files\AspenTech\Aspen HYSYS V12.0\hysys.tlb'  # HYSYS program file location
 
-obj = UnisimConnection(filepath,unisimpath)
+obj = HysysConnection(filepath,Hysyspath)
 obj.OpenCase()
 #%%
 TableDict, Values = obj.ReadDataTable('ProcData1')
 #%%
 from datetime import datetime
 start_time = datetime.now()
-
 Results = []
 Counter = 0
 NaNs = 0
 for x in q:
     obj.WriteTagsDataTable(TableDict,x,'ProcData1')
-    #obj.Reset()     #El tamany de q ha de ser igual als Write i Read/Writes del DataTable
     obj.Run()
-    #if Values == None: #Check if the first column has converged, if it has not, reset and run a new example.
-    #    obj.Reset()
-     #   obj.Run()
-    #else:
     Result, Values = obj.ReadDataTable('ProcData1')
     Results.append(Values)
     Counter = Counter + 1
-    if Values[4] == -32767.0:
-        obj.Reset() #Fem reset quan no ha convergit
+    if Values[4] == -32767.0: #Check if the first column has converged, if it has not, reset and run a new example.
+        obj.Reset() 
         NaNs += 1
-    print(Counter)
-    print(NaNs)
+    print('Number of samples:',Counter)
+    print('Unfeasible samples:',NaNs)
 end_time = datetime.now()
 print(NaNs)
-#%% Proves
+
 import csv 
   
 # field names  
@@ -246,21 +224,17 @@ with open('Model', 'w') as f:
 
 Data_Model  = Results
 
-#%% Proves    
-#print('Results: ', Results)
-
 print('Time:0', end_time-start_time)
 
 #%% 
 #Results
 
 """
-Tenir present que en la lectura de dades les variables temporales passen de h a sec (exemple: duties en kJ/s i flows en kmols/s)
+Variables with units of time are read in sec, disregarding if they are in another unit (such as h) on HYSYS.
 """
 
 tags = list(Result[list(Result.keys())[0]].keys())
 Results=pd.DataFrame(Results)
 Results.columns = tags
-#o = win32com.client.Dispatch("Excel.Application")
-#o.Visible = 1   
+#Save the data into Book1.xlsx 
 Results.to_excel(r'C:\Users\vdi.eebe\Desktop\TFG-main\Book1.xlsx')
